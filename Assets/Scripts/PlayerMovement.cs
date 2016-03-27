6ab902e478a4038c4546594e595841577249ee44
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2016-03-27 18:11:01 +0200

    Added jumping, tweaked movement and added DebugDrawingExtension

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,25 +3,43 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
     public float xMaxVel;
-    private float xDir;
+    public float jumpVel;
     private Rigidbody2D rBody;
+    private float xDir = 0;
+    private float yDir = 0;
+    private float collRadius = 0.51f;
+    private int excludePlayerMask;
 
 	// Use this for initialization
 	private void Awake () {
         rBody = GetComponent<Rigidbody2D>();
+        excludePlayerMask =  ~ (1 << LayerMask.NameToLayer("Player"));
 	}
 	
 	// Update is called once per frame
 	private void FixedUpdate () {
         GetUserInput();
+        Vector2 vel = rBody.velocity;
         if (xDir != 0) {
-            Vector2 vel = rBody.velocity;
             vel.x = xMaxVel * xDir;
-	        rBody.velocity = vel;
         }
+        if (yDir > 0 && TouchingSurface()) {
+            vel.y = jumpVel;
+        }
+	    rBody.velocity = vel;
 	}
 
     private void GetUserInput() {
         xDir = Input.GetAxisRaw("Horizontal");
+        yDir = Input.GetAxisRaw("Vertical");
     }
+
+    private bool TouchingSurface() {
+        Collider2D coll = Physics2D.OverlapCircle(transform.position, collRadius, excludePlayerMask);
+        return coll != null;
+    }
+
+    //public void OnDrawGizmos() {
+    //    DebugExtension.DebugCircle(transform.position, Vector3.forward, Color.yellow, collRadius);
+    //}
 }
