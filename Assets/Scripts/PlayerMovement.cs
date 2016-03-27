73d654b6105c08914c6fe465a99f2d82fdad74d5
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2016-03-27 03:56:11 +0200

    Added base for movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float xMaxVel;
+    private float xDir;
+    private Rigidbody2D rBody;
+
+	// Use this for initialization
+	private void Awake () {
+        rBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate () {
+        GetUserInput();
+        if (xDir != 0) {
+            Vector2 vel = rBody.velocity;
+            vel.x = xMaxVel * xDir;
+	        rBody.velocity = vel;
+        }
+	}
+
+    private void GetUserInput() {
+        xDir = Input.GetAxisRaw("Horizontal");
+    }
+}
