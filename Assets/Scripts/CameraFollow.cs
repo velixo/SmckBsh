502cf6813acd3cab92c076427e67b69467751f7b
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2016-03-28 04:55:22 +0200

    Added camera follow scripts

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject player;
+    public GameObject ground;
+    public GameObject lWall;
+    public GameObject rWall;
+    public float moveSpeed;
+    private float vExtent;
+    private float hExtent;
+    private float xMin;
+    private float xMax;
+    private float yMin;
+	private float yMax;
+
+	// Use this for initialization
+	private void Awake () {
+        vExtent = Camera.main.GetComponent<Camera>().orthographicSize;
+        hExtent = vExtent * Screen.width / Screen.height;
+        xMin = lWall.GetComponent<BoxCollider2D>().bounds.min.x;
+        xMax = rWall.GetComponent<BoxCollider2D>().bounds.max.x;
+        yMin = ground.GetComponent<BoxCollider2D>().bounds.min.y;
+        yMax = lWall.GetComponent<BoxCollider2D>().bounds.max.y;
+	}
+
+	// Update is called once per frame
+	private void Update () {
+        Vector2 newPos = Vector2.Lerp(transform.position, player.transform.position, Time.deltaTime * moveSpeed);
+        newPos.x = Mathf.Clamp(newPos.x, xMin + hExtent, xMax - hExtent);
+        newPos.y = Mathf.Clamp(newPos.y, yMin + vExtent, yMax - vExtent);
+        transform.position = newPos;
+	}
+}
