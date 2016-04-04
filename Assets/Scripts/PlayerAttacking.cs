4928dcff71d84f329bf70f3f65af0becc3d7671c
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2016-04-04 17:34:40 +0200

    Added player hit sprite and hit sprite fade animation

--- Assets/Scripts/PlayerAttacking.cs
+++ Assets/Scripts/PlayerAttacking.cs
@@ -2,52 +2,68 @@
 using System.Collections;
 
 public class PlayerAttacking : MonoBehaviour {
-    public float hitTimeDiff;
+    private bool debug = true;
+    public float minTimeBetweenHits;
     private float lastArrHit = -Mathf.Infinity;
-    private bool debugHitUp = false;
-    private bool debugHitRight = false;
-    private bool debugHitDown = false;
-    private bool debugHitLeft = false;
+    private Vector2 hitDir = Vector2.zero;
+    private GameObject hitSprite = null;
+    private float hitSpriteOffset;
 
-	// Update is called once per frame
+    private void Awake () {
+        hitSpriteOffset = 1f;
+    }
+
 	private void Update () {
         float timeSinceHit = Time.timeSinceLevelLoad - lastArrHit;
-        if (timeSinceHit > hitTimeDiff) {
+        if (timeSinceHit > minTimeBetweenHits) {
             if (Input.GetKey(KeyCode.UpArrow)) {
                 lastArrHit = Time.timeSinceLevelLoad;
-                debugHitUp = true;
+                hitDir = Vector2.up;
             } else if (Input.GetKey(KeyCode.RightArrow)) {
                 lastArrHit = Time.timeSinceLevelLoad;
-                debugHitRight = true;
+                hitDir = Vector2.right;
             } else if (Input.GetKey(KeyCode.DownArrow)) {
                 lastArrHit = Time.timeSinceLevelLoad;
-                debugHitDown = true;
+                hitDir = Vector2.down;
             } else if (Input.GetKey(KeyCode.LeftArrow)) {
                 lastArrHit = Time.timeSinceLevelLoad;
-                debugHitLeft = true;
+                hitDir = Vector2.left;
+            }
+
+            if (hitDir != Vector2.zero) {
+                print("start hit sprite");
+                hitSprite = Instantiate(Resources.Load("Prefabs/HitSprite")) as GameObject;
+                hitSprite.transform.position = transform.position;
+                hitSprite.transform.Translate(hitDir * hitSpriteOffset);
+                hitDir = Vector2.zero;
             }
         }
+
+        CalcHitSpriteAlpha();
 	}
 
+    private void CalcHitSpriteAlpha() {
+        if (hitSprite != null) {
+            Color col = hitSprite.GetComponent<SpriteRenderer>().color;
+            col.a = 1 - Mathf.Clamp01((Time.timeSinceLevelLoad - lastArrHit) / minTimeBetweenHits);
+            if (col.a > 0) {
+                hitSprite.GetComponent<SpriteRenderer>().color = col;
+            } else {
+                Destroy(hitSprite);
+                hitSprite = null;
+            }
+        }
+    }
+
+    private Bounds debugBounds = new Bounds(Vector2.zero, new Vector2(1, 1));
+    private float debugDur = 0.1f;
     public void OnDrawGizmos() {
-        Vector2 extents = new Vector2(1, 1);
-        Vector2 center = transform.position;
-        float dur = 0.1f;
-        if (debugHitUp) {
-            DebugExtension.DebugBounds(new Bounds(center + Vector2.up, extents), Color.green, dur);
-            debugHitUp = false;
-        }
-        if (debugHitRight) {
-            DebugExtension.DebugBounds(new Bounds(center + Vector2.right, extents), Color.green, dur);
-            debugHitRight = false;
-        }
-        if (debugHitDown) {
-            DebugExtension.DebugBounds(new Bounds(center + Vector2.down, extents), Color.green, dur);
-            debugHitDown = false;
-        }
-        if (debugHitLeft) {
-            DebugExtension.DebugBounds(new Bounds(center + Vector2.left, extents), Color.green, dur);
-            debugHitLeft = false;
+        if (debug) {
+            float timeSinceHit = Time.timeSinceLevelLoad - lastArrHit;
+            if (hitDir != Vector2.zero && timeSinceHit > minTimeBetweenHits) {
+                debugBounds.center = (Vector2)transform.position + hitDir * hitSpriteOffset;
+                DebugExtension.DebugBounds(debugBounds, Color.green, debugDur);
+            }
         }
     }
 }
